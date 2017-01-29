469c04db4df16bc04395ca7442bf4bad4c5c912a
NicholasJacques <nicholas.d.jacques@gmail.com>
2017-01-28 20:31:18 -0700

    cleaned up with rubocop

--- credit_check.rb
+++ credit_check.rb
@@ -1,27 +1,27 @@
-card_number = "342804633855673"
+card_number = '342804633855673'
 counter = 0
 sum = 0
 
-card_number = card_number.split""
+card_number = card_number.split''
 
 card_number.reverse_each do |number|
   number = number.to_i
-  #if counter is odd multiple number by two
+  # if counter is odd multiple number by two
   if counter.odd?
     number *= 2
   end
-  #if number  is greater than 9 then add the two digits together
+  # if number  is greater than 9 then add the two digits together
   if number > 9
     number = number.to_s[0].to_i + number.to_s[-1].to_i
   end
 
-  #add number to sum
+  # add number to sum
   sum += number
   counter += 1
 end
 
-if sum % 10 == 0
-  puts "This is a valid card number"
+if (sum % 10).zero?
+  puts 'This is a valid card number'
 else
-  puts "This is an invalid number"
+  puts 'This is an invalid number'
 end
